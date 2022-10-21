98011fe60e02c724440da3b18ab7a1b6453f46d0
Kristy Gislason <kristygislason@gmail.com>
2022-10-21 13:33:56 -0700

    Refactored the code and general clean-up and commenting

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,29 @@
 const request = require('request');
 
-let userArgs = process.argv.slice(2);
+// Allow command line arguments
+const userArgs = process.argv.slice(2);
 
-let catBreed = userArgs[0].toLowerCase().slice(0, 3);
+// First argument is cat breed (string)
+const catBreed = userArgs[0].toLowerCase().slice(0, 3);
 
-let requestURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + catBreed;
+// Add that to the request url query parameters
+const requestURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + catBreed;
 
-console.log(typeof catBreed);
-console.log(requestURL);
+// Make our request to the Cat API
+request(requestURL, function(error, response, body) {
+  // Log errors
+  if (error !== null) {
+    console.error('error:', error);
+  } else {
+    // Parse the JSON string into an object
+    const data = JSON.parse(body);
 
-request(requestURL, function (error, response, body) {
-  //console.error('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(data[0].description);
+    // Check if the array is empty
+    if (data[0].length === 0) {
+      console.log("We did not find a cat breed with that name. Try again!");
+    } else if (data[0].length !== 0) {
+      // If not, grab the description
+      console.log(data[0].description);
+    }
+  }
 });
