f480c5709483550869de4d659d70857d8d4015f4
Kristy Gislason <kristygislason@gmail.com>
2022-10-21 13:53:49 -0700

    Debugging data.length - had data[0].length by accident

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,26 +3,31 @@ const request = require('request');
 // Allow command line arguments
 const userArgs = process.argv.slice(2);
 
-// First argument is cat breed (string)
-const catBreed = userArgs[0].toLowerCase().slice(0, 3);
+// Assign cat breed (string) to variable
+const catBreed = userArgs[0];
+const apiRequestURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+let requestURL = apiRequestURL;
 
-// Add that to the request url query parameters
-const requestURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + catBreed;
+// Check if catBreed is undefined
+if (catBreed !== undefined) {
+  requestURL = apiRequestURL + catBreed;
+}
 
 // Make our request to the Cat API
 request(requestURL, function(error, response, body) {
-  // Log errors
   if (error !== null) {
-    console.error('error:', error);
+    // Log errors
+    console.error('Error: ', error);
   } else {
     // Parse the JSON string into an object
     const data = JSON.parse(body);
 
-    // Check if the array is empty
-    if (data[0].length === 0) {
+    if (data.length === 0) {
+      // Check if the array is empty
       console.log("We did not find a cat breed with that name. Try again!");
     } else if (data[0].length !== 0) {
       // If not, grab the description
+      console.log("You search returned: ", data[0].name);
       console.log(data[0].description);
     }
   }
