7cebddd4f1d78c5411ca2dbf46c898cad72dc81b
Kristy Gislason <kristygislason@gmail.com>
2022-10-21 14:39:10 -0700

    Refactor code per instructions - spilt into index and breedFetcher files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,27 @@
 const request = require('request');
-
-// Allow command line arguments
-const userArgs = process.argv.slice(2);
-
-// Assign cat breed (string) to variable
-const catBreed = userArgs[0];
 const apiRequestURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-let requestURL = apiRequestURL;
 
-// Check if catBreed is undefined
-if (catBreed !== undefined) {
-  requestURL = apiRequestURL + catBreed;
-}
+const fetchBreedDescription = (breedName, callback) => {
+  if (!breedName) return;
 
-// Make our request to the Cat API
-request(requestURL, function(error, response, body) {
-  if (error !== null) {
-    // Log errors
-    console.error('Error: ', error);
-  } else {
-    // Parse the JSON string into an object
+  // Make our request to the Cat API
+  request((apiRequestURL + breedName), function(error, response, body) {
+    // Parse JSON String
     const data = JSON.parse(body);
 
+    // Check if it is empty
     if (data.length === 0) {
       // Check if the array is empty
       console.log("We did not find a cat breed with that name. Try again!");
-    } else if (data[0].length !== 0) {
-      // If not, grab the description
-      console.log("You search returned: ", data[0].name);
-      console.log(data[0].description);
+      return;
     }
-  }
-});
+
+    // Get the description   
+    const desc = data[0].description;
+
+    // Call the callback:
+    callback(error, desc);
+  });
+};
+
+module.exports = { fetchBreedDescription };
